9696065701a7098405b233c69aa50a2824b7ee80
Donghun Cha <75019048+Joseph-Cha@users.noreply.github.com>
2022-09-09 22:15:30 +0900

    #9 리스트 뷰 복습 및 리스트 뷰 아이템 클릭 이벤트 처리

---
+++ AndroidProject/5th/listView_re/app/src/main/java/com/donghun/listview_re/ListViewModel.kt
@@ -0,0 +1,6 @@
+package com.donghun.listview_re
+
+data class ListViewModel (
+    val text1 : String,
+    val text2 : String
+)
---
+++ AndroidProject/5th/listView_re/app/src/main/java/com/donghun/listview_re/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.donghun.listview_re
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val list_item = mutableListOf<String>()
+//
+//        list_item.add("A")
+//        list_item.add("B")
+//        list_item.add("C")
+
+        val list_item2 = mutableListOf<ListViewModel>()
+        list_item2.add(ListViewModel("a", "b"))
+        list_item2.add(ListViewModel("c", "d"))
+        list_item2.add(ListViewModel("e", "f"))
+
+        val listView = findViewById<ListView>(R.id.mainListView)
+
+        val listviewAdapter = ListViewAdapter(list_item2)
+        listView.adapter = listviewAdapter
+
+        listView.setOnItemClickListener { adapterView, view, position, id ->
+//            Toast.makeText(this, list_item[position], Toast.LENGTH_LONG).show()
+        }
+    }
+}
