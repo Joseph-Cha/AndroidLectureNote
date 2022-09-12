6a5133f5d13102ff73112a2505a0a291b1e8873e
Joseph.c <joseph.c@kakao.com>
2022-09-12 12:43:08 +0900

    #9 트로트앱 만들기_레이아웃 설정

---
+++ AndroidProject/5th/trot_list/app/src/main/java/com/example/trot_list/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.trot_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 1. Navigation 만들기
+        // 2. Fragment 만들고 연결하기
+            // 2.1 Fragment에서 이미지 클릭 시 이동
+        // 3. 리사이클러뷰 만들기
+    }
+}
