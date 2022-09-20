d9a199d755390de70c1b539a6705f0fb3ccf3210
Joseph.c <joseph.c@kakao.com>
2022-09-20 21:38:07 +0900

    #15 파이어베이스 리얼타임 데이터베이스(Realtime Database) 저장하기

--- AndroidProject/6th/diet_memo/app/src/main/java/com/example/diet_memo/MainActivity.kt
+++ AndroidProject/6th/diet_memo/app/src/main/java/com/example/diet_memo/MainActivity.kt
@@ -7,8 +7,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
             val mAlertDialog = mBuilder.show()
 
             val dateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+            var dateText = ""
 
             dateSelectBtn!!.setOnClickListener {
                 val today = GregorianCalendar()
@@ -40,9 +44,26 @@ class MainActivity : AppCompatActivity() {
                         dayOfMonth: Int) {
                         Log.d("MAIN","${year}, ${month + 1}, ${dayOfMonth}")
                         dateSelectBtn.setText("${year}/${month + 1}/${dayOfMonth}")
+                        dateText = "${year}/${month + 1}/${dayOfMonth}"
                     }
                 }, year, month, date)
                 dlg.show()
+            }
+
+            val saveBtn = mAlertDialog.findViewById<Button>(R.id.saveBtn)
+            saveBtn?.setOnClickListener {
+
+                val healthMeno = mAlertDialog.findViewById<EditText>(R.id.healthMemo)!!.text.toString()
+
+                // Write a message to the database
+                val database = Firebase.database
+                val myRef = database.getReference("myMeno")
+
+                val model = DataModel(dateText, healthMeno)
+
+                myRef
+                    .push()
+                    .setValue(model)
             }
         }
     }
