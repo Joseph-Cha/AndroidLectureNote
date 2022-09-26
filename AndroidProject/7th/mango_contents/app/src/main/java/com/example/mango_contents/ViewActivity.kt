7e919d53c1c46e64e813042bd36f63a43129f685
Joseph.c <joseph.c@kakao.com>
2022-09-26 21:22:29 +0900

    #21  데이터베이스에 북마크 정보 저장하기 & 이메일 로그인 연동

--- AndroidProject/7th/mango_contents/app/src/main/java/com/example/mango_contents/ViewActivity.kt
+++ AndroidProject/7th/mango_contents/app/src/main/java/com/example/mango_contents/ViewActivity.kt
@@ -3,14 +3,38 @@ package com.example.mango_contents
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ViewActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view)
 
         val webView = findViewById<WebView>(R.id.webView)
         webView.loadUrl(intent.getStringExtra("url").toString())
 
+        // Write a message to the database
+        val database = Firebase.database
+        val myBookmarkRef = database.getReference("message")
+
+        val url = intent.getStringExtra("url")
+        val title = intent.getStringExtra("title")
+        val imageUrl = intent.getStringExtra("imageUrl")
+
+        val savedText = findViewById<TextView>(R.id.saveText)
+        savedText.setOnClickListener {
+            myBookmarkRef
+                .child(auth.currentUser!!.uid)
+                .push()
+                .setValue(ContentsModel(url.toString(), imageUrl.toString(), title.toString()))
+        }
     }
 }
