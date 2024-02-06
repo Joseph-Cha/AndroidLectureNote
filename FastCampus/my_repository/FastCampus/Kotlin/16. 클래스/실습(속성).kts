ff977dcb7fd1682c87c7b53ac92fb1291bcf2431
Joseph.c <joseph.c@kakao.com>
2024-02-06 22:52:50 +0900

    클래스 lateinit 실습 수강 및 정리 완료

--- FastCampus/my_repository/FastCampus/Kotlin/16. 클래스/실습(속성).kts
+++ FastCampus/my_repository/FastCampus/Kotlin/16. 클래스/실습(속성).kts
@@ -13,6 +13,21 @@ class Book() {
 }
 
 val book = Book()
-println(book.title) // 책 제목 -> 초기화보다 getter가 더 나중에 호출됨
-book.title = "제목 변경"
-println(book.title) // 제목 변경
+//println(book.title) // 책 제목 -> 초기화보다 getter가 더 나중에 호출됨
+//book.title = "제목 변경"
+//println(book.title) // 제목 변경
+
+class Book2() {
+    lateinit var title: String
+    fun nextPage() {
+        if (::title.isInitialized) {
+            println("페이지가 넘어간다")
+        } else {
+            println("초기화 필요")
+        }
+    }
+}
+
+val book2 = Book2()
+book2.title = "책이름"
+book2.nextPage()
